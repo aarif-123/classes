29913224a8cf0ac56b58af4261af7c7c01ca140b
Mohammed Aarif <mohdaarif92422@gmail.com>
2026-03-26 19:52:05 +0530

    fixed vending machine

---
+++ main.java
@@ -0,0 +1,26 @@
+public class main {
+    private String state;
+
+    public main() {
+        this.state = "IDLE"; // initial state
+    }
+
+    public void next() {
+        if (state.equals("IDLE")) {
+            state = "HAS_MONEY";
+            System.out.println("Money inserted. Select product.");
+        } 
+        else if (state.equals("HAS_MONEY")) {
+            state = "DISPENSING";
+            System.out.println("Product is being dispensed...");
+        } 
+        else if (state.equals("DISPENSING")) {
+            state = "IDLE";
+            System.out.println("Product delivered. Back to idle state.");
+        }
+    }
+
+    public String getState() {
+        return state;
+    }
+}
